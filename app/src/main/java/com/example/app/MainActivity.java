16b796c7915b179477c904558643d2e0ca00b04b
Tan <tolasuunhj98@gmail.com>
2020-06-13 04:52:51 +0700

    Initial Android

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -11,15 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String hoten ="Nguyen Duy Tan";
-        //Show in logcat
-        Log.d("Hello",hoten);
-
-        int a=1;
-        while(a<10){
-            Log.d("Tan",""+a);
-            a++;
-        }
     }
 }
